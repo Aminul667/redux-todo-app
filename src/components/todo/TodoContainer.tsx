496c7d2c97eb46342687200f8854afcdea419087
aminul <rahathssn@gmail.com>
2024-08-10 02:17:36 +0600

    added linear gradient

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,3 +1,5 @@
+import TodoCard from "./TodoCard";
+
 const TodoContainer = () => {
   return (
     <div>
@@ -5,10 +7,13 @@ const TodoContainer = () => {
         <button>Add todo</button>
         <button>Filter</button>
       </div>
-      <div className="bg-red-500 w-full h-[500px] rounded-xl p-5 space-y-3">
-        <div className="bg-white p-5 flex justify-center items-center rounded-md text-2xl font-bold">
+      <div className="bg-primary-gradient w-full h-full rounded-xl p-5 space-y-3">
+        <TodoCard></TodoCard>
+        <TodoCard></TodoCard>
+        <TodoCard></TodoCard>
+        {/* <div className="bg-white p-5 flex justify-center items-center rounded-md text-2xl font-bold">
           <p>There is no tasks pending</p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
