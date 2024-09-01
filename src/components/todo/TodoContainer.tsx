89d60f0ff757cb3a4254c8acb461036295f6a130
aminul <rahathssn@gmail.com>
2024-09-01 16:55:20 +0600

    added Handle Modal

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,13 +1,13 @@
-import { Button } from "../ui/button";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
   return (
     <div>
       <div className="flex justify-between mb-5">
         <AddTodoModal></AddTodoModal>
-        <Button>Filter</Button>
+        <TodoFilter></TodoFilter>
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
         <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
